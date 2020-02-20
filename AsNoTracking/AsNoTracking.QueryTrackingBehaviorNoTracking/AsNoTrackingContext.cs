dbc9f9e5a65a417d083f0c5581b40a976c834605
Igor Rončević <ironcev@hotmail.com>
2020-02-20 12:24:55 +0100

    Refactor AsNoTracking.QueryTrackingBehaviorNoTracking

--- AsNoTracking/AsNoTracking.QueryTrackingBehaviorNoTracking/AsNoTrackingContext.cs
+++ AsNoTracking/AsNoTracking.QueryTrackingBehaviorNoTracking/AsNoTrackingContext.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityFrameworkCoreHiddenGems;
+using Microsoft.EntityFrameworkCore;
 
 namespace AsNoTracking
 {
-    public class AsNoTrackingContext : DbContext
+    public class AsNoTrackingContext : BaseDemoDbContext
     {
-        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder
-                .UseSqlServer(
-                    @"Server=(localdb)\mssqllocaldb;Database=EntityFrameworkCoreHiddenGems.AsNoTracking;Trusted_Connection=True;ConnectRetryCount=0;")
+            base.OnConfiguring(optionsBuilder);
 
-                // .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-                ;
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
     }
 }
